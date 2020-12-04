4a0ab1fad074ac9b2ee3d19d542e83599360caeb
poychang <poypost@gmail.com>
2020-12-04 13:39:06 +0800

    init project

---
+++ DemoDynamicDataModelWebAPI/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoDynamicDataModelWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FormController : ControllerBase
+    {
+        private readonly ILogger<FormController> _logger;
+
+        public FormController(ILogger<FormController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("OK");
+        }
+
+        // POST /form/{type}/
+        [HttpPost]
+        [Route("{type}")]
+        public IActionResult Data([FromRoute] string type, [FromBody] dynamic query)
+        {
+            return type switch
+            {
+                "BForm" => Ok($"{(string)query.Title} - {(string)query.Description}"),
+                "AForm" => Ok($"{(string)query.Name} {query.Number}"),
+                _ => Ok("Not support this form."),
+            };
+        }
+    }
+}
